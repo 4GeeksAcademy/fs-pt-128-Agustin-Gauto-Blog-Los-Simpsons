710eba202b70f016dd3e044680169b46869ce9dd
AgustinMGauto <agustin_gauto@outlook.com>
2026-01-14 11:25:30 +0000

    casi casi

--- src/Services/APIServices.js
+++ src/Services/APIServices.js
@@ -5,3 +5,9 @@ export const getCharacters = async (dispatch) => {
         console.log(data);
         dispatch({ type: "get_characters", payload: data.results })
 }
+
+export const getCharacter = async (id, dispatch) => {
+    const response = await fetch(`https://thesimpsonsapi.com/api/characters${id}`);
+        const data = await response.json()
+        return data
+}
--- src/pages/CardBig.jsx
+++ src/pages/CardBig.jsx
@@ -1,22 +1,36 @@
 import { Link, useParams } from "react-router-dom"
 import useGlobalReducer from "../hooks/useGlobalReducer"
+import { useEffect, useState } from "react"
+import { getCharacter } from "../Services/APIServices"
 
 export const CardBig = () => {
 
+    const { store, dispatch } = useGlobalReducer()
 
     const {id} = useParams()
+    
+    const [character, setCharacter ] = useState({})
 
-    const { store, dispatch } = useGlobalReducer()
+    const getCharacterData = async () => { 
+        const characterData = await getCharacter(id)
+        setCharacter(characterData)
+    }
+
+    useEffect(()=>{
+        getCharacterData()
+      },[])
+   
+
 
     return (
         <>
     
             <div className="card m-auto" style={{width: "18rem"}}>
                 <div className="card-body">
-                    <div className="">
+                    <div className="d-flex justify-content-between">
                         <img src={`https://cdn.thesimpsonsapi.com/500/character/${id}.webp`} alt="" />
                         <div>
-                            <h1>TITULO</h1>
+                            <h1>{character.name}</h1>
                             <p>texto</p>
                         </div>
                     </div>
